9a8262f0d78e54c8a7bd22a36cf942b46629b8c1
talesa365 <talesahowell7@gmail.com>
2022-10-21 18:14:01 -0400

    error on 35

--- build_library.py
+++ build_library.py
@@ -6,7 +6,7 @@ NOTE: ALL methods for the Library class go in the library.py script
 '''
 
 # 1.0 TODO: Import the Library class
-
+from library import Library 
 
 # 2.1 TODO: In `library.py`:
 #   Write the 'constructor' method of the Library class to have one attribute `books`, an empty list.
@@ -16,21 +16,26 @@ NOTE: ALL methods for the Library class go in the library.py script
 # 2.2 TODO: In this file:
 #   Create an instance of the `Library` class called `my_library`.
 
+my_library = Library()
 
 # 2.3 TODO: In this file:
 #   Print the `books` attribute of `my_library`. It should be an empty list.
-
+print(my_library.books)
 
 # 2.4 TODO: In this file:
 #   Also print out the `type` of `my_library` and the `type` of its `books` attribute
-
-
+print(type(my_library))
+print(type(my_library.books))
 # 3.1 TODO: In `library.py`:
 #   Fill out the `add()` method to add a book to an instance of the Library class.
 #   - The method should take two parameters (other than self): `title` and `author`, both strings
 #   - The method should create a `Book` object (it's already imported into the file) with those two attributes
 #   - Then, the method should append this book to the `books` attribute of the `Library` object
-
+book = my_library
+book.add_title(" The Body Keeps the Score - Bessel van der Kolk")
+book.add_title("Sapiens - Yuval Noah Harari")
+book.add_title(" Braiding Sweetgrass - Robin Wall Kimmerer")
+book.add_title("  The Warmth of Other Suns - Isabel Wilkerson")
 
 # 3.2 TODO: In this file:
 #   Once you have finished writing the `add_title()` method above, add the following books to your library:
@@ -43,20 +48,19 @@ NOTE: ALL methods for the Library class go in the library.py script
 
 # 3.3 TODO: Print the `books` attribute of my_library again to make sure your books have been added.
 
-
+print(my_library.books)
 # 3.4 TODO: Huh. That looks a little weird... Loop through the `books` attribute and print each
 #   one out separately instead.
-
+for book in my_library.books:
+    print(f'{book}\n')
 
 # 4.1 TODO: In `library.py`:
 #   Fill out the `count_books` method to get the number of books in an instance of the `Library` class.
 #   - The method should only take in the `self` parameter, no others
 #   - The method should return the length of the `books` attribute as an integer
-
-
 # 4.2 TODO: In this script:
 #   Once you have finished the method, count the books in my_library and print out the result
-
+print(my_library.count_books())
 
 # 5.1 TODO: In `library.py`:
 #   Fill out the `remove_title` method to remove a book (by title) from an instance of the Library class.
@@ -64,7 +68,7 @@ NOTE: ALL methods for the Library class go in the library.py script
 #   - The method should remove all books matching the input title from the `Library` instance.
 #       NOTE: We know a `list` method that does something like this, but will it remove ALL matching books?
 #   - The method does not need to return anything
-
+print(my_library.remove_title('Braiding Sweetgrass' ))
 
 # 5.2 TODO: In this script:
 #   Once you have finished the method, remove 'Braiding Sweetgrass' from `my_library`.
@@ -72,7 +76,8 @@ NOTE: ALL methods for the Library class go in the library.py script
 
 # 5.3 TODO: In this script:
 #   Repeat the loop you wrote in 3.4 to check if it worked.
-
+for book in my_library.books:
+    print(f'{book}\n')
 
 # We've now used that loop twice, and any time we want to see the titles in our library,
 #   we'll probably want to use it again. That tells us something: the loop code is a pretty good
@@ -88,7 +93,7 @@ NOTE: ALL methods for the Library class go in the library.py script
 
 # 6.2 TODO: In this file:
 #   This time, call your library's `display_books` method to see if it works.
-
+print(my_library.display_books())
 
 # 7.1 TODO: In this script:
 #   Instantiate another instance of the `Library` class called `nyt_bestsellers`
--- library.py
+++ library.py
@@ -1,23 +1,36 @@
-from book import Book
 
+
+from book import Book
 
 class Library():
     def __init__(self):
         """Initialize the empty book list"""
-        pass
+        self.books = []
+     
 
-    def add_title(self):
+    def add_title(self, title, author):
         """Add a Book object with the given title and author to the book list"""
-        pass
-
+        book = {}
+        book["title" ]= title
+        book["author"]= author
+        book = Book (title, author)
+        self.books.append(book)
+        
     def count_books(self):
         """Return the number of books currently in the booklist"""
-        pass
+        return len(self.books)
 
-    def remove_title(self):
+    def remove_title(self, title):
         """Remove a book from the book list"""
-        pass
+        for book in self.books:
+            if book.title == title:
+                self.books.remove(book)
+        return self.books
 
     def is_empty(self):
         """Return True if the book list is empty, False if not"""
         return self.books == []
+    
+    def display_books(self):
+        for book in self.books:
+            print(f'{book.title} - {book.author}')
